ec820a6fd7fabbff2ccd9a50e3eaa9b513f11b49
TonyJem <a.jemanov@gmail.com>
2021-01-22 20:50:00 +0200

    Copy project files

---
+++ blueCirclesGameXIB/CircleView.swift
@@ -0,0 +1,94 @@
+import UIKit
+
+class CircleView: UIView {
+    
+    @IBOutlet private weak var xibCircleBody: UIView!
+    @IBOutlet private weak var circleLabel: UILabel!
+    
+    private var workingView: UIView!
+    private var xibName: String = "CircleView"
+    
+    private var curentValue: Int {
+        get {
+            guard let curentValue = Int(circleLabel.text!) else { return 0 }
+            return curentValue
+        }
+        set(curentValue) {
+            circleLabel.text = String(curentValue)
+            circleLabel.font = UIFont.systemFont(ofSize: radius)
+        }
+    }
+    
+    private var radius: CGFloat = 50 {
+        didSet{
+            self.frame.size.width = 2 * radius
+            self.frame.size.height = 2 * radius
+            self.layer.zPosition += 5
+            xibCircleBody.layer.cornerRadius = radius
+        }
+    }
+    
+    private var area: CGFloat {
+        return pow(radius, 2) * CGFloat.pi
+    }
+    
+    private var absorbDistance: CGFloat {
+        return radius
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setCustomView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setCustomView()
+    }
+    
+    private func getFromXib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let xib = UINib(nibName: xibName, bundle: bundle)
+        let view = xib.instantiate(withOwner: self, options: nil).first as! UIView
+        view.layer.cornerRadius = view.frame.size.width / 2
+        return view
+    }
+    
+    private func setCustomView() {
+        workingView = getFromXib()
+        workingView.frame = bounds
+        workingView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        addSubview(workingView)
+    }
+    
+    func canAbsorb(_ otherCircle: CircleView) -> Bool {
+        let deltaX = otherCircle.center.x - self.center.x
+        let deltaY = otherCircle.center.y - self.center.y
+        let distanceBetweenCenters = (pow(deltaX, 2) + pow(deltaY, 2)).squareRoot()
+        
+        return distanceBetweenCenters <= absorbDistance
+    }
+    
+    func absorb(_ otherCircle: CircleView) {
+        let commonArea = otherCircle.area + area
+        let newRadius = (commonArea/CGFloat.pi).squareRoot()
+        otherCircle.isHidden = true
+        addValue(from: otherCircle)
+        setRadius(to: newRadius)
+        setBackGroundColor(with: .blue)
+    }
+    
+    private func addValue(from otherCircle: CircleView) {
+        let newValue = curentValue + otherCircle.curentValue
+        curentValue = newValue
+    }
+    
+    private func setRadius(to newRadius: CGFloat) {
+        radius = newRadius
+    }
+    
+    private func setBackGroundColor(with newColor: UIColor) {
+        xibCircleBody.backgroundColor = newColor
+    }
+}
---
+++ blueCirclesGameXIB/ViewController.swift
@@ -0,0 +1,98 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet private weak var circle1: CircleView!
+    @IBOutlet private weak var circle2: CircleView!
+    @IBOutlet private weak var circle3: CircleView!
+    @IBOutlet private weak var circle4: CircleView!
+    @IBOutlet private weak var circle5: CircleView!
+    @IBOutlet private weak var circle6: CircleView!
+    @IBOutlet private weak var circle7: CircleView!
+    @IBOutlet private var circles: [CircleView]!
+    
+    var activeCircles = [CircleView]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activeCircles = circles
+    }
+    
+    @IBAction private func panCircle1Action(_ gesture: UIPanGestureRecognizer) {
+        move(circle1, with: gesture)
+    }
+    
+    @IBAction private func panCircle2Action(_ gesture: UIPanGestureRecognizer) {
+        move(circle2, with: gesture)
+    }
+    
+    @IBAction private func panCircle3Action(_ gesture: UIPanGestureRecognizer) {
+        move(circle3, with: gesture)
+    }
+    
+    @IBAction private func panCircle4Action(_ gesture: UIPanGestureRecognizer) {
+        move(circle4, with: gesture)
+    }
+    
+    @IBAction private func panCircle5Action(_ gesture: UIPanGestureRecognizer) {
+        move(circle5, with: gesture)
+    }
+    
+    @IBAction private func panCircle6Action(_ gesture: UIPanGestureRecognizer) {
+        move(circle6, with: gesture)
+    }
+    
+    @IBAction private func panCircle7Action(_ gesture: UIPanGestureRecognizer) {
+        move(circle7, with: gesture)
+    }
+    
+    private func roundCorners(for circles: [CircleView]) {
+        for circle in circles {
+            circle.layer.cornerRadius = circle.frame.size.width / 2
+        }
+    }
+    
+    private func move(_ movedCircle: CircleView, with gesture: UIPanGestureRecognizer) {
+        let gestureTranslation = gesture.translation(in: view)
+        guard let gestureView = gesture.view else { return }
+        
+        gestureView.center = CGPoint(
+            x: gestureView.center.x + gestureTranslation.x,
+            y: gestureView.center.y + gestureTranslation.y
+        )
+        
+        gesture.setTranslation(.zero, in: view)
+        guard gesture.state == .ended else { return }
+        
+        absorbIfCan(movedCircle)
+    }
+    
+    private func absorbIfCan(_ movedCircle: CircleView) {
+        for circle in activeCircles {
+            if circle == movedCircle { continue }
+            
+            if circle.canAbsorb(movedCircle) {
+                circle.absorb(movedCircle)
+                removeFromCircles(view: movedCircle)
+                break
+            }
+        }
+    }
+    
+    private func removeFromCircles(view: CircleView) {
+        for (index, circle) in activeCircles.enumerated() {
+            if circle == view {
+                activeCircles.remove(at: index)
+                if activeCircles.count == 1 {
+                    moveLastCircleToCenter()
+                }
+                break
+            }
+        }
+    }
+    
+    private func moveLastCircleToCenter() {
+        activeCircles[0].center.x = self.view.center.x
+        activeCircles[0].center.y = self.view.center.y
+    }
+}
